1342f8e9f91bf3f6a01955bb46e4a4e612aba4e8
Mbnoob <maharajabain190@gmail.com>
2022-11-22 13:21:33 +0530

    updated 22/11/2022

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const joi = require("joi");
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 
 //..................................................MySQL Connections..........................................
